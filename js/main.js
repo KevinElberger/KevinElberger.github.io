e99d7ab982c5c7bb5f38dd5011079892cd49cb19
kevin elberger <kevelberger@gmail.com>
2016-08-17 16:44:56 -0700

    Add responsive navbar, additional styling required

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
+    // Fixed navigation menu
+    var secondaryHead = $('nav');
+    var secondaryHeadTop = secondaryHead.offset().top;
+    $(window).on('scroll', function() {
+        if($(window).scrollTop() > secondaryHeadTop) {
+            secondaryHead.addClass("fixed-nav");
+            $("nav ul").addClass("fixed-links");
+        } else {
+            secondaryHead.removeClass("fixed-nav");
+            $("nav ul").removeClass("fixed-links");
+        }
+    });
+
+    // On arrow click, scroll down
     $('.main-title').delay(1000).animate({"opacity": "1"}, 700);
     $(function() {
         $(window).scroll(function() {
@@ -10,11 +24,12 @@ $(document).ready(function() {
             }
         });
 
+        // Scroll animation, to href position
         $('.jumper').on("click", function(e) {
             e.preventDefault();
 
             $("body, html").animate({
-                scrollTop: $($(this).attr('href')).offset().top -60
+                scrollTop: $($(this).attr('href')).offset().top
             }, 600);
         });
     })
