60c9195ce7eaa57e467b6eb137893b9dabb09730
kevin elberger <kevelberger@gmail.com>
2016-08-19 12:10:30 -0700

    Add scroll easing animation for navbar clicks

--- js/main.js
+++ js/main.js
@@ -38,5 +38,16 @@ $(document).ready(function() {
                 scrollTop: $($(this).attr('href')).offset().top
             }, 600);
         });
-    })
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 });
